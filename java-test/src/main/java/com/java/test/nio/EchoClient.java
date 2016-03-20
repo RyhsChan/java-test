4df8061579701eade2139d69d703bb4453790751
walter <cxq1127@163.com>
2016-03-20 10:25:28 +0800

    +) add asynchronous socket EchoClient

--- java-test/src/main/java/com/java/test/nio/EchoClient.java
+++ java-test/src/main/java/com/java/test/nio/EchoClient.java
@@ -1,10 +1,49 @@
 package com.java.test.nio;
 
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.nio.channels.CompletionHandler;
+
 public class EchoClient {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public void doEcho(String server, int port) throws Exception {
 
+		AsynchronousSocketChannel clientChannel = AsynchronousSocketChannel
+				.open();
+		ByteBuffer sndBuffer = ByteBuffer.wrap(new String("Hello, Server")
+				.getBytes());
+		ByteBuffer rcvBuffer = ByteBuffer.allocate(1024);
+		clientChannel.connect(new InetSocketAddress(server, port)).get();
+		if (clientChannel.write(sndBuffer).get() != sndBuffer.position()) {
+			System.out.println("Send error");
+			return;
+		}
+
+		// CompletionHandler implementation
+		clientChannel.read(rcvBuffer, rcvBuffer,
+				new CompletionHandler<Integer, ByteBuffer>() {
+
+					public void completed(Integer result, ByteBuffer attachment) {
+						System.out.println("Got " + result + " bytes: "
+								+ new String(attachment.array()));
+					}
+
+					public void failed(Throwable exc, ByteBuffer attachment) {
+						System.out.println("Receive error");
+					}
+
+				});
+	}
+
+	public static void main(String[] args) {
+
+		try {
+			new EchoClient().doEcho("127.0.0.1", 8080);
+			Thread.sleep(3000);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
