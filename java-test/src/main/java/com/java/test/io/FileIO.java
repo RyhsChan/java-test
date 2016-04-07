d6f44427f5371c23841464631834841bebc64841
RyhsChan <cxq1127@163.com>
2016-04-07 10:51:22 +0800

    *) charset support

--- java-test/src/main/java/com/java/test/io/FileIO.java
+++ java-test/src/main/java/com/java/test/io/FileIO.java
@@ -2,18 +2,22 @@ package com.java.test.io;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.LineNumberReader;
+import java.nio.ByteOrder;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Vector;
 
 interface FilePrinter {
 	void printFile();
-
-	void closeFile() throws IOException;
-
-	void openFile() throws IOException;
 }
 
 abstract class AbstractFilePrinter implements FilePrinter {
@@ -46,8 +50,13 @@ abstract class AbstractFilePrinter implements FilePrinter {
 
 	abstract public void closeFile() throws IOException;
 
-	public AbstractFilePrinter(String filePath) {
+	public AbstractFilePrinter(String filePath, Charset charset) {
 		this.filePath = filePath;
+		this.charset = charset != null ? charset : Charset.defaultCharset();
+	}
+
+	public Charset getCharset() {
+		return charset;
 	}
 
 	public String getFilePath() {
@@ -55,32 +64,66 @@ abstract class AbstractFilePrinter implements FilePrinter {
 	}
 
 	private String filePath;
+	private Charset charset;
 }
 
 class StreamFilePrinter extends AbstractFilePrinter {
 	private InputStream in = null;
 
-	public StreamFilePrinter(String filePath) {
-		super(filePath);
+	public StreamFilePrinter(String filePath, Charset charset) {
+		super(filePath, charset);
+	}
+
+	private byte[] toBytes(Vector<Byte> vec) {
+		int i = 0;
+		byte[] bytes = new byte[vec.size()];
+		for (Byte b : vec) {
+			bytes[i++] = b;
+		}
+		return bytes;
 	}
 
 	@Override
 	String readLine() throws IOException {
-		StringBuilder builder = new StringBuilder();
-		byte b;
+		Vector<Byte> byteArray = new Vector<Byte>();
 		do {
-			b = (byte) in.read();
-			if (b == -1 || b == '\n') {
+			int n = in.read();
+			if (n == -1 || n == '\n') {
 				break;
 			}
-			builder.append((char) b);
+			byteArray.add((byte) n);
 		} while (true);
 
-		if (builder.length() > 0) {
-			if (builder.charAt(builder.length() - 1) == '\r') {
-				builder.deleteCharAt(builder.length() - 1);
+		if (byteArray.size() > 0) {
+			if (getCharset().equals(StandardCharsets.UTF_16LE)
+					|| (getCharset().equals(StandardCharsets.UTF_16) && ByteOrder
+							.nativeOrder().equals(ByteOrder.LITTLE_ENDIAN))) {
+				if (byteArray.size() > 1
+						&& byteArray.elementAt(byteArray.size() - 1) == '\0'
+						&& byteArray.elementAt(byteArray.size() - 2) == '\r') {
+					byteArray.setSize(byteArray.size() - 2);
+				}
+				return new String(toBytes(byteArray), getCharset());
+			} else if (getCharset().equals(StandardCharsets.UTF_16BE)
+					|| (getCharset().equals(StandardCharsets.UTF_16) && ByteOrder
+							.nativeOrder().equals(ByteOrder.BIG_ENDIAN))) {
+				if (byteArray.size() > 0
+						&& byteArray.elementAt(byteArray.size() - 1) == '\0') {
+					byteArray.setSize(byteArray.size() - 1);
+				}
+				if (byteArray.size() > 1
+						&& byteArray.elementAt(byteArray.size() - 1) == '\r'
+						&& byteArray.elementAt(byteArray.size() - 2) == '\0') {
+					byteArray.setSize(byteArray.size() - 2);
+				}
+				return new String(toBytes(byteArray), getCharset());
 			}
-			return builder.toString();
+			if (byteArray.size() > 0
+					&& byteArray.elementAt(byteArray.size() - 2) == '\r') {
+				byteArray.setSize(byteArray.size() - 1);
+			}
+
+			return new String(toBytes(byteArray), getCharset());
 		}
 		return null;
 	}
@@ -103,8 +146,8 @@ class BufferedFilePrinter extends AbstractFilePrinter {
 
 	private BufferedReader in = null;
 
-	public BufferedFilePrinter(String filePath) {
-		super(filePath);
+	public BufferedFilePrinter(String filePath, Charset charset) {
+		super(filePath, charset);
 	}
 
 	@Override
@@ -121,18 +164,23 @@ class BufferedFilePrinter extends AbstractFilePrinter {
 
 	public void openFile() throws IOException {
 		closeFile();
-		in = new LineNumberReader(new FileReader(getFilePath()));
+		in = new LineNumberReader(new InputStreamReader(new FileInputStream(
+				getFilePath()), getCharset()));
 	}
 }
 
 public class FileIO {
 	public static final String filePath = System.getProperty(
-			"resource.filepath", "D:\\dns.txt");
+			"resource.filepath", "D:\\unicode.txt");
+	public static final String charset = System.getProperty("resource.charset",
+			"UTF-16");
 
 	public static void main(String[] args) {
-		FilePrinter filePrinter = new StreamFilePrinter(filePath);
-		FilePrinter filePrinterEx = new BufferedFilePrinter(filePath);
+		FilePrinter filePrinter = new StreamFilePrinter(filePath,
+				Charset.forName(charset));
 		filePrinter.printFile();
+		FilePrinter filePrinterEx = new BufferedFilePrinter(filePath,
+				Charset.forName(charset));
 		filePrinterEx.printFile();
 	}
 }
